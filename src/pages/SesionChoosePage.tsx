2313abe70240cb6890c14021f49fd36f3827050b
Battusay <you@example.com>
2025-06-07 21:33:35 +0200

    Antes de continuar crear el propt mode en ListPrises.tsx

--- src/pages/SesionChoosePage.tsx
+++ src/pages/SesionChoosePage.tsx
@@ -3,17 +3,19 @@ import PraiseSearch from "../components/PraiseSearch";
 import { useState } from "react";
 import PraiseInterface from "../interface/PraiseInterface";
 import TypePraise from "../components/TypePraise";
+import ListPraises from "../components/ListPraises";
 
 
 export default function SesionChoosePage() {
 
     const [sending, setSending] = useState(false);
     const[praises, setPraises] = useState<PraiseInterface[]>();
+    const[typePraise,setTypePraise] = useState("");
     
     return(
         <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
             <PraiseSearch sending={sending} setPraises={setPraises} setSending={setSending}/>
-            <TypePraise/>
+            {typePraise===""?<TypePraise setType={setTypePraise}/>:<ListPraises type={typePraise}/>}
         </Box>
     );
 }
